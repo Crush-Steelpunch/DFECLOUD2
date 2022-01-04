165255e6bb8c1c625232784ca10e5a594e176542
Leon L. Robinson <leon.robinson@qa.com>
2022-01-04 11:31:29 +0000

    OOP example

---
+++ budget.py
@@ -0,0 +1,46 @@
+import pdb
+
+pdb.set_trace()
+
+class Catagory:
+
+    # we need a balance attribute
+    def __init__(self,balance=0):
+        self.balance = balance
+    
+
+    # we need a deposit function
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+    # we need a withdrawl function
+    def withdrawl(self, amount):
+        self.balance = self.balance - amount
+
+    # we need a transfer function
+    # where from <- the balance of the other catagory object
+    # where to <- The object that this method is called from
+    # Amount
+    def transfer(self,amount,fromcatagory):
+        fromcatagory.withdrawl(amount)
+        self.balance = self.balance + amount
+
+food = Catagory(0)
+clothes = Catagory(15)
+
+print('#Food Account')
+print(food.balance)
+food.deposit(15)
+print(food.balance)
+
+food.transfer(15, clothes)
+
+print(food.balance)
+
+
+#food.transfer()
+
+print(clothes.balance)
+clothes.deposit(60)
+print(clothes.balance)
+clothes.withdrawl(12)
+print(clothes.balance)
